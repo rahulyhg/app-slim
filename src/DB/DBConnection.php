251dcdb0db7fd7c614acca712554acd155c19d59
ricardoapf <antonio@elaniin.com>
2019-01-10 19:12:01 -0600

    fix procfile

--- src/DB/DBConnection.php
+++ src/DB/DBConnection.php
@@ -2,13 +2,14 @@
 
 class DBConnection
 {
-  private $host = 'localhost:3306';
-  private $database = 'slimapi';
-  private $username = 'root';
-  private $password = 'wp6883';
+  private $host = $_ENV['DB_HOST'];
+  private $database = $_ENV['DB_DATABASE'];
+  private $username = $_ENV['DB_USERNAME'];
+  private $password = $_ENV['DB_PASSWORD'];
 
   public function mConnect(){
     $conn_mysql = "mysql:host=$this->host;dbname=$this->database";
+    
     $connDB = new PDO($conn_mysql, $this->username, $this->password);
     $connDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
